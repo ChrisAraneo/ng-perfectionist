840f4b387fb85b9461808f77df9a0198cf6410ca
Krzysztof Pająk <chris.araneo@gmail.com>
2026-04-05 23:22:02 +0200

    feat: getDecoratorFirstArg util function

---
+++ src/utils/get-decorator-first-arg.ts
@@ -0,0 +1,17 @@
+import { AST_NODE_TYPES, type TSESTree } from '@typescript-eslint/utils';
+import { match, P } from 'ts-pattern';
+import { get } from 'lodash-es';
+
+export const getDecoratorFirstArg = (
+  node: TSESTree.Decorator,
+): TSESTree.ObjectExpression | undefined =>
+  match(node.expression)
+    .with(
+      {
+        type: AST_NODE_TYPES.CallExpression,
+        arguments: [{ type: AST_NODE_TYPES.ObjectExpression }, ...P.array()],
+      },
+      (expression) =>
+        get(expression, 'arguments[0]') as TSESTree.ObjectExpression,
+    )
+    .otherwise(() => undefined);
