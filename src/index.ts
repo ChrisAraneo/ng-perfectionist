4ec43a630487120b010a82c3be8e24dbb2891f11
Krzysztof Pająk <chris.araneo@gmail.com>
2026-04-06 22:25:21 +0200

    feat: add rule to index plugin file, bump version

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import type { ESLint, Rule } from 'eslint';
 
 import { sortComponentImports } from './rules/sort-component-imports.js';
+import { sortDirectiveImports } from './rules/sort-directive-imports.js';
 
 const rules: Record<string, Rule.RuleModule> = {
   'sort-component-imports': sortComponentImports as unknown as Rule.RuleModule,
+  'sort-directive-imports': sortDirectiveImports as unknown as Rule.RuleModule,
 };
 
 const plugin: ESLint.Plugin = {
   meta: {
     name: '@chris.araneo/eslint-plugin-ng-perfectionist',
-    version: '0.0.1',
+    version: '0.1.0',
   },
   rules,
 };
