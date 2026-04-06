cd0424e84bb7c84b6302b06fc01dd706f245fc92
Krzysztof Pająk <chris.araneo@gmail.com>
2026-04-06 14:16:15 +0200

    feat: getNonNullElements util function

---
+++ src/utils/get-non-null-elements.ts
@@ -0,0 +1,7 @@
+import { type TSESTree } from '@typescript-eslint/utils';
+import { compact, isUndefined } from 'lodash-es';
+
+export const getNonNullElements = (
+    array: TSESTree.ArrayExpression | undefined,
+): (TSESTree.Expression | TSESTree.SpreadElement)[] | undefined =>
+    isUndefined(array?.elements) ? undefined : (compact(array.elements) as (TSESTree.Expression | TSESTree.SpreadElement)[]);
