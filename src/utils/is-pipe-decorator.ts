e5d357592f031cda1adc895fa40f0d6f02591f2b
Krzysztof Pająk <chris.araneo@gmail.com>
2026-04-07 23:07:39 +0200

    feat: isPipeDecorator function

---
+++ src/utils/is-pipe-decorator.ts
@@ -0,0 +1,6 @@
+import { AST_NODE_TYPES, type TSESTree } from '@typescript-eslint/utils';
+
+export const isPipeDecorator = (node: TSESTree.Decorator): boolean =>
+  node.expression.type === AST_NODE_TYPES.CallExpression &&
+  node.expression.callee.type === AST_NODE_TYPES.Identifier &&
+  node.expression.callee.name === 'Pipe';
