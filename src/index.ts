73e0f6e7094f5156c0545057f811a6cfd57cbd8a
Krzysztof Pająk <chris.araneo@gmail.com>
2026-04-07 21:03:28 +0200

    feat: add @NgModule import sorting rule

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import type { ESLint, Rule } from 'eslint';
 
 import { sortComponentImports } from './rules/sort-component-imports.js';
 import { sortDirectiveImports } from './rules/sort-directive-imports.js';
+import { sortNgModuleImports } from './rules/sort-ng-module-imports.js';
 
 const rules: Record<string, Rule.RuleModule> = {
   'sort-component-imports': sortComponentImports as unknown as Rule.RuleModule,
   'sort-directive-imports': sortDirectiveImports as unknown as Rule.RuleModule,
+  'sort-ng-module-imports': sortNgModuleImports as unknown as Rule.RuleModule,
 };
 
 const plugin: ESLint.Plugin = {
